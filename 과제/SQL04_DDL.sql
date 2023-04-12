e4b1ea4a37f96ebb6468900d4e970bbedfd14d94
GDJ <GDJ@DESKTOP-NAPOHR9>
2023-04-12 20:09:38 +0900

    SQL04_DDL 과제

--- 과제/SQL04_DDL.sql
+++ 과제/SQL04_DDL.sql
@@ -87,17 +87,55 @@ ALTER TABLE TB_DEPARTMENT
 
 --10. 춘 기술대학교 학생들의 정보만이 포함되어 있는 학생일반정보 VIEW를 만들고자 한다.
 --아래 내용을 참고하여 적절한 SQL문을 작성하시오.
+CREATE VIEW VW_학생일반정보
+AS SELECT STUDENT_NO,STUDENT_NAME,STUDENT_ADDRESS
+FROM TB_STUDENT;
+
+SELECT * FROM VW_학생일반정보;
 
 
 --11. 춘 기술대학교는 1년에 두번씩 과별로 학생과 지도교수가 지도 면담을 진행한다.
 --이를 위해 사용할 학생이름, 학과이름, 담당교수이름 으로 구성되어 있는 VIEW를 만드시오.
 --이때 지도 교수가 없는 학생이 있을 수 있음을 고려하시오 (단, 이 VIEW 는 단순SELECT 만을 할 경우 학과별로 정렬되어 화면에 보여지게 만드시오.)
+CREATE VIEW VW_지도면담
+AS SELECT STUDENT_NAME AS 학생이름,DEPARTMENT_NAME AS 학과이름,PROFESSOR_NAME AS 지도교수이름
+FROM TB_STUDENT 
+    JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+    LEFT JOIN TB_PROFESSOR ON PROFESSOR_NO = COACH_PROFESSOR_NO;
+
+SELECT * FROM VW_지도면담;    
+        
 
 -- 12 .모든 학과의 학과별 학생 수를 확인할 수 있도록 적절한 VIEW를 작성해 보자.
+CREATE VIEW VW_학과별학생수
+AS SELECT DEPARTMENT_NAME,COUNT(STUDENT_NO) AS 학생수
+FROM TB_STUDENT 
+    JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+GROUP BY DEPARTMENT_NAME;    
+
+SELECT * FROM VW_학과별학생수;  
+
 
 -- 13. 위에서 생성한 학생일반정보 VIEW를 통해서 학번이 A213046인 학생의 이름을 본인 이름으로 변경하는 SQL문을 작성하시오.
+UPDATE VW_학생일반정보 SET STUDENT_NAME = '최인호' WHERE STUDENT_NO = 'A213046';
+SELECT * FROM VW_학생일반정보 WHERE STUDENT_NO = 'A213046';
 
 -- 14.13번에서와 같이 VIEW를 통해서 데이터가 변경될 수 있는 상황을 막으려면 VIEW를 어떻게 생성해야 하는지 작성하시오.
+CREATE OR REPLACE VIEW VW_학생일반정보
+AS SELECT STUDENT_NO,STUDENT_NAME,STUDENT_ADDRESS
+FROM TB_STUDENT
+WITH READ ONLY;
+
+UPDATE VW_학생일반정보 SET STUDENT_NAME = '최인호' WHERE STUDENT_NO = 'A213046';
 
 -- 15. 춘 기술대학교는 매년 수강신청 기간만 되면 특정 인기 과목들에 수강 신청이 몰려 문제가 되고 있다.
 -- 최근 3년을 기준으로 수강 인원이 가장 많았던 3 과목을 찾는 구문을 작성해보시오.
+SELECT *
+FROM(
+    SELECT C.CLASS_NO AS 과목번호,C.CLASS_NAME AS 과목이름, COUNT(*) AS "누적수강생(명)"
+    FROM TB_CLASS  C
+         JOIN TB_GRADE G ON C.CLASS_NO=G.CLASS_NO   
+    WHERE TERM_NO LIKE '2005%' OR TERM_NO LIKE '2006%' OR TERM_NO LIKE '2007%' OR TERM_NO LIKE '2008%' OR TERM_NO LIKE '2009%' 
+    GROUP BY C.CLASS_NO,C.CLASS_NAME
+    ORDER BY 3 DESC)
+WHERE ROWNUM <=3; 
